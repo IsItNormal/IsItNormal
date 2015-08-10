2c450daefd342750edb9e6d0e3d693078ce194e1
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-10 15:11:31 -0700

    Serves up recent posts and saves submitted posts

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var upload = require("multer")({dest: "uploads/"});
-var Post = require("../db/modesl/post.js");
+var Post = require("../../db/models/post.js");
 
 router.get('/', function(req, res){
   Post.find({}).limit(20).sort('-date').exec(function(err, posts){
@@ -19,8 +19,22 @@ router.get('/:id', function(req, res){
 });
 
 router.post('/', upload.single('picture'), function(req, res){
-  console.log(req.file);
-  res.send(req.file);
+  var post = new Post({
+    image_path: req.destination,
+    description: req.params.description,
+    meta: {
+      normal_votes: 0,
+      not_normal_votes: 0
+    }
+  });
+
+  post.save(function(err) {
+    if(err){
+      res.send(500);
+    } else {
+      res.send(200);
+    }
+  });
 
 });
 module.exports = router;
