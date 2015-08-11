2a92dc4c4fbebdca788c72ebacc6c1ec6e49cff8
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-10 18:08:23 -0700

    Correct string concatenation

--- client/views/UploadForm.js
+++ client/views/UploadForm.js
@@ -1,11 +1,11 @@
 var UploadForm = Backbone.View.extend({
-  template: _.template("<form action='http:localhost:3000/api/posts \
-                          enctype='multipart/form-data \
-                          method='POST'> \
-                          <input type='file' name='picture' /><br /> \
-                          <input type='text' name='description' /><br /> \
-                          <button action='submit'>Upload!</button> \
-                        </form>"),
+  template: _.template("<form action='http:localhost:3000/api/posts" +
+                         "enctype='multipart/form-data" +
+                         "method='POST'>" +
+                         "<input type='file' name='picture' /><br />" +
+                         "<input type='text' name='description' /><br />" +
+                         "<button action='submit'>Upload!</button>" +
+                       "</form>"),
   render: function(){
     this.$el.append(this.template());
   },
