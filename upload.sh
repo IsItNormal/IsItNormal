d81d7e0df9ca6c13213ca29ab2512feb4e597853
Andrew Weber <andrew.robert.weber@gmail.com>
2015-08-10 17:40:24 -0700

    fixes syntax error in posts and correctly serves static upload folder

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-curl -v -F description="Totally normal crab person, amirite?" -F picture=@$PWD/$1 http://localhost:3000/api/posts
+curl -v -F description="Totally normal crab person, amirite?" -F picture=@"/Users/weber/Desktop/IsItNormal"/$1 http://localhost:3000/api/posts
