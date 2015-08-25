1d3536e9c680d234be68d63835f585a2ac0e167e
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-24 19:48:04 -0700

    Properly defined, instantiantiated and hooked up router

--- client/router.js
+++ client/router.js
@@ -1,23 +1,23 @@
-var Router = Backbone.Router.extend({
-
+var AppRouter = Backbone.Router.extend({
   routes: {
-    '/:id':             'landing',
-    '/profile/:id':      'profile',
+    "/profile/:id": "profile",
+    ":id": "picture",
+    "*actions": "default"
   },
-  
-  landing: function(id) {
-    if(id) {
-      console.log("Landing called with: ",id);
-    } else {
-      console.log("Landing called");
-    }
-  },
-
-  profile: function(id) {
-    if(id) {
-      console.log("Profile called with: ",id);
-    } else {
-      console.log("Profile called");
-    }
-  }
 });
+
+var NormalRouter = new AppRouter;
+
+NormalRouter.on('route:picture', function(id) {
+  console.log("Landing called with: ", id);
+});
+
+NormalRouter.on("route:profile", function(id) {
+  console.log("Profile called with: ", id);
+});
+
+NormalRouter.on("route:default", function() {
+  console.log("Default route");
+});
+
+Backbone.history.start();
