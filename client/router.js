5344fdcb5cfe58020a052a5211390d3683d91ddb
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-24 21:04:13 -0700

    Gave route handlers better names and corrected syntax to provide expected behavior

--- client/router.js
+++ client/router.js
@@ -1,28 +1,31 @@
 var AppRouter = Backbone.Router.extend({
   routes: {
-    "/profile/:id": "profile",
-    "upload": "upload",
-    ":id": "picture",
+    "profile": "viewProfile",
+    "profile/:id": "viewProfile",
+    "upload": "handleUpload",
+    ":id": "viewPicture",
     "*actions": "default"
   },
 });
 
 var NormalRouter = new AppRouter;
 
-NormalRouter.on('route:picture', function(id) {
-  console.log("Landing called with: ", id);
-});
 
-NormalRouter.on("route:profile", function(id) {
+
+NormalRouter.on("route:viewProfile", function(id) {
   console.log("Profile called with: ", id);
 });
 
-NormalRouter.on("router:upload", function(id) {
+NormalRouter.on("route:handleUpload", function(id) {
   console.log("Sorry, no html upload form yet!");
 });
 
-NormalRouter.on("route:default", function() {
-  console.log("Default route");
+NormalRouter.on('route:viewPicture', function(id) {
+  console.log("Landing called with: ", id);
+});
+
+NormalRouter.on("route:default", function(actions) {
+  console.log("Default route ", actions );
 });
 
 Backbone.history.start();
